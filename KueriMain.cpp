7b8feaa3bf637c33869d23c55e22bb279f8ceabc
Verideth <geckoman009@gmail.com>
2016-11-21 18:14:59 -0500

    Update KueriMain.cpp

--- KueriMain.cpp
+++ KueriMain.cpp
@@ -100,7 +100,7 @@ void __stdcall CreateMove(int sequence_number, float input_sample_frametime, boo
 	int nLocalPlayerID = engine->GetLocalPlayer();
 	VeriSDK::ClientEntity* pLocal = static_cast<VeriSDK::ClientEntity*>(entList->GetClientEntity(nLocalPlayerID));
 
-	if (cmd->buttons & IN_JUMP && !(pLocal->GetFlags() & FL_ONGROUND))
+	if (cmd->buttons & IN_JUMP && !(pLocal->GetFlags() & FL_ONGROUND)) // added this to prove that createmove is working
 	{
 		engine->SetViewAngles(VeriSDK::Vector(25, 0, 25));
 	}
